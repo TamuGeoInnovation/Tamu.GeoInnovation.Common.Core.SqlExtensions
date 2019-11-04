ef3f821538571da01f2e5bd31a5b04eb85dfde0f
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2019-11-04 16:10:26 -0600

    updated json utils

--- Src/Main/SoundexExtensions/SoundexEncoderExtension.cs
+++ Src/Main/SoundexExtensions/SoundexEncoderExtension.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Text;
-using Microsoft.SqlServer.Server;
 
 namespace USC.GISResearchLab.Common.Core.SqlExtensions.SoundexExtensions
 {
